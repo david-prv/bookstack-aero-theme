8f72113783d1b75e38e9d1328a7e8a6d850ddc5b
david-prv <david_dewes@web.de>
2024-06-03 15:20:56 +0300

    now displaying page IDs in Page Editor (supporting german and english for now

---
+++ aero_theme/entities/meta.blade.php
@@ -0,0 +1,105 @@
+<div class="entity-meta">
+    @if ($entity->id)
+        <div class="entity-meta-item">
+            @icon('info')
+            <div>
+                @php
+                    $classPathArray = explode('\\',get_class($entity));
+                    $objectType = "entities.".strtolower(end($classPathArray));
+                @endphp
+                {!! trans('entities.meta_id_name', [
+                    'id' => '<span title="'. $entity->id . '">' . $entity->id. '</span>',
+                    'resourceName' => '<span title="'. trans($objectType) . '">' . trans($objectType) . '</span>',
+                ]) !!}
+            </div>
+        </div>
+    @endif
+
+    @if($entity->isA('revision'))
+        <div class="entity-meta-item">
+            @icon('history')
+            <div>
+                {{ trans('entities.pages_revision') }}
+                {{ trans('entities.pages_revisions_number') }}{{ $entity->revision_number == 0 ? '' : $entity->revision_number }}
+            </div>
+        </div>
+    @endif
+
+    @if ($entity->isA('page'))
+        <a href="{{ $entity->getUrl('/revisions') }}" class="entity-meta-item">
+            @icon('history'){{ trans('entities.meta_revision', ['revisionCount' => $entity->revision_count]) }}
+        </a>
+    @endif
+
+    @if ($entity->ownedBy && $entity->owned_by !== $entity->created_by)
+        <div class="entity-meta-item">
+            @icon('user')
+            <div>
+                {!! trans('entities.meta_owned_name', [
+                    'user' => "<a href='{$entity->ownedBy->getProfileUrl()}'>".e($entity->ownedBy->name). "</a>"
+                ]) !!}
+            </div>
+        </div>
+    @endif
+
+    @if ($entity->createdBy)
+        <div class="entity-meta-item">
+            @icon('star')
+            <div>
+                {!! trans('entities.meta_created_name', [
+                    'timeLength' => '<span title="'.$entity->created_at->toDayDateTimeString().'">'.$entity->created_at->diffForHumans() . '</span>',
+                    'user' => "<a href='{$entity->createdBy->getProfileUrl()}'>".e($entity->createdBy->name). "</a>"
+                ]) !!}
+            </div>
+        </div>
+    @else
+        <div class="entity-meta-item">
+            @icon('star')
+            <span title="{{$entity->created_at->toDayDateTimeString()}}">{{ trans('entities.meta_created', ['timeLength' => $entity->created_at->diffForHumans()]) }}</span>
+        </div>
+    @endif
+
+    @if ($entity->updatedBy)
+        <div class="entity-meta-item">
+            @icon('edit')
+            <div>
+                {!! trans('entities.meta_updated_name', [
+                    'timeLength' => '<span title="' . $entity->updated_at->toDayDateTimeString() .'">' . $entity->updated_at->diffForHumans() .'</span>',
+                    'user' => "<a href='{$entity->updatedBy->getProfileUrl()}'>".e($entity->updatedBy->name). "</a>"
+                ]) !!}
+            </div>
+        </div>
+    @elseif (!$entity->isA('revision'))
+        <div class="entity-meta-item">
+            @icon('edit')
+            <span title="{{ $entity->updated_at->toDayDateTimeString() }}">{{ trans('entities.meta_updated', ['timeLength' => $entity->updated_at->diffForHumans()]) }}</span>
+        </div>
+    @endif
+
+    @if($referenceCount ?? 0)
+        <a href="{{ $entity->getUrl('/references') }}" class="entity-meta-item">
+            @icon('reference')
+            <div>
+                {{ trans_choice('entities.meta_reference_count', $referenceCount, ['count' => $referenceCount]) }}
+            </div>
+        </a>
+    @endif
+
+    @if($watchOptions?->canWatch())
+        @if($watchOptions->isWatching())
+            @include('entities.watch-controls', [
+                'entity' => $entity,
+                'watchLevel' => $watchOptions->getWatchLevel(),
+                'label' => trans('entities.watch_detail_' . $watchOptions->getWatchLevel()),
+                'ignoring' => $watchOptions->getWatchLevel() === 'ignore',
+            ])
+        @elseif($watchedParent = $watchOptions->getWatchedParent())
+            @include('entities.watch-controls', [
+                'entity' => $entity,
+                'watchLevel' => $watchOptions->getWatchLevel(),
+                'label' => trans('entities.watch_detail_parent_' . $watchedParent->type . ($watchedParent->ignoring() ? '_ignore' : '')),
+                'ignoring' => $watchedParent->ignoring(),
+            ])
+        @endif
+    @endif
+</div>
---
+++ aero_theme/lang/de/entities.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'meta_id_name' => ':resourceName #:id',
+];
